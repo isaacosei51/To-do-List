ede3addc16845ee1eac71b074b91a9b492473bfa
isaacosei51 <oseisaac51@gmail.com>
2023-08-09 17:16:46 +0000

    add:new changes

--- index.js
+++ index.js
@@ -3,17 +3,39 @@ let add = document.querySelector("#addBtn");
 let itemsContainer = document.querySelector(".to-do-list");
 let listItem = localStorage.listItem ? JSON.parse(localStorage.listItem) : [];
 
+const checkControl=(index)=>{
+  listItem[index].completed=!listItem[index].completed
+  localStorage.setItem("listItem",JSON.stringify(listItem));
+  newList()
+}
+
+const converted=(index)=>{
+ const stage= new Date(listItem[index].timestamp);
+ console.log(stage)
+ return `${stage.toLocaleString()}`
+}
+
 const newList = () => {
   itemsContainer.innerHTML = ``;
   for (let i = listItem.length - 1; i >= 0; i--) {
+    let itemChecked=""
+    if (listItem[i].completed) {
+      itemChecked="checked"
+    }else{
+      itemChecked=""
+    }
+    //  const timestamp = new Date(listItem[i].timestamp);
+    // <p>${timestamp.toLocaleString()}</p>
     itemsContainer.innerHTML += `
-        <li class="item">
-        <p>${listItem[i]}</p>
-          <div>
-        <button onclick ="editTask(${i})">Edit</button>
-        <button onclick ="deleteItem(${i})">Delete</button>
-        </div>
-        </li>
+    <li class="item ${itemChecked}">
+    <p>${listItem[i].name}</p>
+    <p>${converted(i)}</p>
+    <div>
+    <button onclick ="editTask(${i})">Edit</button>
+    <button onclick ="deleteItem(${i})">Delete</button>
+    <input type="checkbox" ${itemChecked}   onchange="checkControl(${i})">
+    </div>
+    </li>
     `;
   }
 };
@@ -21,7 +43,12 @@ const newList = () => {
 const addItem = (item) => {
   if (item.length > 0) {
     // console.log("fuck")
-    listItem.push(item);
+    let newItem = {
+      name: item,
+      timestamp: Date.now(),
+      completed: false,
+    };
+    listItem.push(newItem);
     console.log(item);
     console.log(listItem);
     input.value = "";
